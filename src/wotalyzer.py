b63faf3035a592577c3735f5705dd2acfb23a78e
Marko Poutiainen <marko.poutiainen@ixonos.com>
2011-02-25 16:00:11 +0200

    wotalyzer now prints something.

--- src/wotalyzer.py
+++ src/wotalyzer.py
@@ -9,8 +9,6 @@ import pprint
 import csv
 import getopt
 import sys
-import itertools
-from copy import deepcopy
 
 def print_tanks(tanks):
     pp = pprint.PrettyPrinter(indent=4)
@@ -22,26 +20,25 @@ def usage():
     print "Figure it out!"
 
 def top_values(tanks):
-    tank = list(itertools.islice(tanks,1,2))
-    
-    best_tanks = dict({'turret_front':tank, 'turret_sides':tank, 'turret_back':tank, 
+    tank = list(tanks)[0]
+    best_tanks = dict({'HP':tank, 'MaxSpeed':tank, 'turret_front':tank, 'turret_sides':tank, 'turret_back':tank, 
                        'hull_front':tank, 'hull_sides':tank, 'hull_back':tank,
                        'penetration':tank, 'damage':tank, 'traverse':tank})
     
-    fields = ['turret_front', 'turret_sides', 'turret_back', 
-              'hull_front', 'hull_sides', 'hull_back',
-              'penetration', 'damage', 'traverse']
+    fields = ['HP', 'MaxSpeed', 'penetration', 'damage', 'traverse',
+              'turret_front', 'turret_sides', 'turret_back', 
+              'hull_front', 'hull_sides', 'hull_back']
     
     for tank in tanks:
         for field in fields:
             if tank[field] > best_tanks[field][field]:
                 best_tanks[field] = tank
 
-    print best_tanks['turret_front']
-    print "Best tanks by values:\n"
+    print "\nBest tanks by values:\n"
+    print "Name\t\t\t\ttier\t\tvalue\n\n"
     for field in fields:
         tank = best_tanks[field] 
-        print field + ": " + tank['full_name'] + "(" + tank[field] + ")" 
+        print field + ": " + tank['full_name'] + '\t\t[' + tank['tier'] + "]\t\t (" + tank[field] + ")" 
         
             
 def filter_func(tank, use_tanks, target):
@@ -95,7 +92,7 @@ def main(argv):
     # Analyze selected tanks    
     
     # First let's find out the best values for armor, penetration, speed, traverse and damage
-    top_values(tanks)
+    top_values(selected)
         
     
 if __name__ == '__main__':
